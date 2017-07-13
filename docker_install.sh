7b37f71d45eee1273b698e9fb66735562317cfd4
David Robinson <david.pl.robinson@googlemail.com>
2017-07-13 16:08:03 +1000

    Update docker_install.sh

--- docker_install.sh
+++ docker_install.sh
@@ -8,7 +8,6 @@ apt-get install -y --no-install-recommends apt-transport-https ca-certificates c
 curl -fsSL https://apt.dockerproject.org/gpg | sudo apt-key add -
 add-apt-repository "deb https://apt.dockerproject.org/repo/ ubuntu-$(lsb_release -cs) main"
 apt-get update
-#apt-get -y install docker-engine
 apt-get -y install docker-compose
 curl -L "https://github.com/docker/compose/releases/download/1.10.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/bin/docker-compose
 chmod +x /usr/bin/docker-compose
